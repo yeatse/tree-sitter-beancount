e7da71690a78beefea4b8f5102f694411a8433c4
Yang Chao <iyeatse@gmail.com>
2024-04-14 02:01:40 +0800

    add highlight file

---
+++ queries/highlights.scm
@@ -0,0 +1,49 @@
+; Basic elements
+((bool) @boolean)
+((date) @constant)
+((key) @property)
+((tag) @markup)
+((link) @markup.link)
+((string) @string)
+((currency) @string)
+((flag) @string.special.symbol)
+((account) @variable)
+
+[
+  (number)
+  (paren_num_expr)
+  (unary_num_expr)
+  (binary_num_expr)
+] @number
+
+; Highlighting directives as keywords
+[
+  "INCLUDE"
+  "OPTION"
+  "PLUGIN"
+  "PUSHTAG"
+  "POPTAG"
+  "PUSHMETA"
+  "POPMETA"
+  "BALANCE"
+  "CLOSE"
+  "COMMODITY"
+  "CUSTOM"
+  "DOCUMENT"
+  "EVENT"
+  "NOTE"
+  "OPEN"
+  "PAD"
+  "PRICE"
+  "TXN"
+  "QUERY"
+] @keyword
+
+; Operators
+[
+  "@" "@@"
+] @operator
+
+[
+  "{" "}" "{{" "}}"
+] @punctuation.bracket
